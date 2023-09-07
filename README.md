ef9996ab0a3bb50921143135bb40eb5861d70539
Vladimir Vorozbit <vorozbitcrypto7@gmail.com>
2023-09-07 16:59:22 +0200

    Update by fetch und cheerio

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Node.js Meme Scraper
 
 ... running node index.js should ...
+
 command line tool to scrape memes from "memegen-link-examples-upleveled.netlify.app"
+...
+
+Own notes:
 
 1. directory created through command: mkdir memes
 2. add new directory to .gitignore: just by typing "memes" into any line followed by a slash / directory "memes" went light grey as the directory "node_modules"
@@ -9,3 +13,6 @@ command line tool to scrape memes from "memegen-link-examples-upleveled.netlify.
    - fetch method (left it cause way complicated )
    - library for accessing :
      - https://www.npmjs.com/package/axios
+4. find a way to filter/split the necessary information from the Html code through library "Cheerio" or "JSDOM" --> cheerio way is not working ...
+5. convert the strings (Url) into arrays
+6. use loops for getting necessary information into an array
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-import axios from 'axios';
+import axios from 'axios'; // Don´t used, cause interface appears in green und not fully, still aproximately 400 charachters left
+import * as cheerio from 'cheerio';
+import got from 'got'; // just tried that from my google research
 
 // Accessing the website, using the .get and .then method of the library
+
+fetch('https://memegen-link-examples-upleveled.netlify.app/')
+  .then((response) => response.text())
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
+
+/*
 axios
   .get('https://memegen-link-examples-upleveled.netlify.app/')
   .then(function (response) {
@@ -10,14 +19,19 @@ axios
     console.log(error);
   })
   .finally(function () {});
-
-// GET request for remote image in node.js
-/*
-  axios({
-  method: 'get',
-  url: 'https://memegen-link-examples-upleveled.netlify.app/',
-  responseType: 'stream',
-}).then(function (response) {
-  response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'));
-});
 */
+
+/*
+got(url)
+  .then((response) => {
+    const $ = cheerio.load(response.body);
+    console.log($('div')[0]);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+*/
+
+const $ = cheerio.load('<div>...</div>', null, false); // even <section id="images"...> not work to filter the elements. Still showing the whole html code
+$.html(); // run the function
+// $('div');
